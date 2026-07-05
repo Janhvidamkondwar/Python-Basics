2110edb560e0d454ea42ba398fd5571e5df0ca22
Janhvi Damkondwar <janhvidamkondwar@gmail.com>
2026-07-06 00:37:20 +0530

    Day02 - Added Python List append() example

---
+++ Day02 /list_append.py
@@ -0,0 +1,6 @@
+# Day 2 of python list append
+
+fruits = ['Apple ',' Orange ', 'Banana ',' Mango ']
+print ("Before append  : "  ,fruits )     
+fruits.append('Water-Melon')
+print("After append : " , fruits)
